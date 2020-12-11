5e6eaaadde2e2d8823764d3f1dbe7c89a4c13c28
Ketrin <ketrin632@gmail.com>
2020-12-11 21:12:21 +0200

    first commit

---
+++ ColorModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ColorViewer
+{
+    public class ColorModel : INotifyPropertyChanged
+    {
+        private byte alpha;
+        public byte Alpha
+        {
+            get { return alpha; }
+            set
+            {
+                if (alpha != value)
+                {
+                    alpha = value;
+                    Color = Color.FromArgb(alpha, red, green, blue);
+                    OnPropertyChanged(nameof(Color));                   
+                    OnPropertyChanged();                   
+                }
+            }
+        }
+
+        private byte red;
+        public byte Red
+        {
+            get { return red; }
+            set
+            {
+                if (red != value)
+                {
+                    red = value;
+                    Color = Color.FromArgb(alpha, red, green, blue);
+                    OnPropertyChanged(nameof(Color));
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private byte green;
+        public byte Green
+        {
+            get { return green; }
+            set
+            {
+                if (green != value)
+                {
+                    green = value;
+                    Color = Color.FromArgb(alpha, red, green, blue);
+                    OnPropertyChanged(nameof(Color));
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private byte blue;
+        public byte Blue
+        {
+            get { return blue; }
+            set
+            {
+                if (blue != value)
+                {
+                    blue = value;
+                    Color = Color.FromArgb(alpha, red, green, blue);
+                    OnPropertyChanged(nameof(Color));
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private Color _color;
+        public Color Color
+        {
+            get { return _color; }
+            set
+            {
+                if (_color != value)
+                {
+                    _color = value;
+                    Color = Color.FromArgb(alpha, red, green, blue);
+                    OnPropertyChanged(nameof(Color));
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ColorModel()
+        {
+            Alpha = 0;
+            Red = 0;
+            Green = 0;
+            Blue = 0;           
+        }
+
+        public object Clone()
+        {            
+            return (ColorModel)this.MemberwiseClone();           
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+
+   
+}
---
+++ ViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorViewer
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private readonly ICollection<ColorModel> colors = new ObservableCollection<ColorModel>();
+        public IEnumerable<ColorModel> Colors => colors;
+
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ColorModel selectedColor;
+        public ColorModel SelectedColor
+        {
+            get { return selectedColor; }
+            set
+            {
+                if (value != selectedColor)
+                {
+                    selectedColor = value;
+                    OnPropertyChanged();
+                }
+
+            }
+        }
+        public ViewModel()
+        {
+            SelectedColor = new ColorModel();
+            
+        }
+        //public bool IsTheSameMethod()
+        //{
+        //    bool isTheSame = true;
+        //    if (colors != null)
+        //    {
+        //        foreach (var item in colors)
+        //        {
+        //            if (item == SelectedColor)
+        //            {
+        //                isTheSame = false;
+        //                break;
+        //            }
+        //            else
+        //                isTheSame = true;
+        //        }
+        //    }
+        //    return isTheSame;
+        //}
+
+
+        public void AddColor()
+        {
+            if (SelectedColor != null)
+            {
+                colors.Add((ColorModel)SelectedColor.Clone());
+            }
+        }
+
+
+        public void RemoveSelectedColor()
+        {
+            if (SelectedColor != null)
+            {
+
+                colors.Remove(SelectedColor);
+                SelectedColor = new ColorModel();
+
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
